38e461aaa8290917738b512d454a2501f5c1a4a2
Alex <alex.kryzhny@mail.ru>
2020-02-27 23:24:19 +0300

    feat: recursive-depth solved

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,17 +1,12 @@
 module.exports = class DepthCalculator {
-    constructor() {
-        this.maxDepth = 0;
-        this.depth = 0;
-    }
+  calculateDepth(arr) {
+    let depth = 0;
+    arr.forEach(element => {
+      if (Array.isArray(element)) {
+        depth = Math.max(this.calculateDepth(element), depth);
+      }
+    });
 
-    calculateDepth(arr) {
-        this.depth++;
-        this.maxDepth = Math.max(this.maxDepth, this.depth);
-        arr.forEach(i => {
-            if (Array.isArray(i))
-                this.calculateDepth(i)
-        });
-        this.depth -= 1;
-        return this.maxDepth;
-    }
+    return ++depth;
+  }
 };
