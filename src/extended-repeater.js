d3c70a6eb83ed8e5bd6e98ada9a65b6e26a244e9
Alex <alex.kryzhny@mail.ru>
2020-02-28 19:29:25 +0300

    feat: extended-repeater solved

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,20 +1,26 @@
 module.exports = function repeater(str, options) {
-    const {repeatTimes = 0, separator = '+', addition, additionRepeatTimes = 0, additionSeparator = '|'} = options;
-    let ret = '';
-    for (let i = 0; i < repeatTimes; i++) {
-        ret += str;
-        if (addition) {
-            for (let j = 0; j < additionRepeatTimes; j++) {
-                ret += addition;
-                if (j < additionRepeatTimes - 1) {
-                    ret += additionSeparator;
-                }
-            }
-        }
-        if (i < repeatTimes - 1) {
-            ret += separator;
-        }
+  const {
+    repeatTimes = 1,
+    separator = "+",
+    addition,
+    additionRepeatTimes = 1,
+    additionSeparator = "|"
+  } = options;
+
+  let ret = "";
+
+  for (let i = 0; i < repeatTimes; i++) {
+    ret += str;
+
+    if (addition !== undefined) {
+      for (let j = 0; j < additionRepeatTimes; j++) {
+        ret += addition;
+        j < additionRepeatTimes - 1 ? (ret += additionSeparator) : null;
+      }
     }
-    return ret;
+
+    i < repeatTimes - 1 ? (ret += separator) : null;
+  }
+
+  return ret;
 };
-  
