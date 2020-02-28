905998c4de26dd4f07a935854c9499b5d973e636
MetaVII <forro22@gmail.com>
2020-02-28 17:34:41 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,20 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    const {repeatTimes = 0, separator = '+', addition, additionRepeatTimes = 0, additionSeparator = '|'} = options;
+    let ret = '';
+    for (let i = 0; i < repeatTimes; i++) {
+        ret += str;
+        if (addition) {
+            for (let j = 0; j < additionRepeatTimes; j++) {
+                ret += addition;
+                if (j < additionRepeatTimes - 1) {
+                    ret += additionSeparator;
+                }
+            }
+        }
+        if (i < repeatTimes - 1) {
+            ret += separator;
+        }
+    }
+    return ret;
 };
   
