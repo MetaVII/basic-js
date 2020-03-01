f043936b91881e1fc4369580577994f725e87899
Alex <alex.kryzhny@mail.ru>
2020-03-01 21:16:35 +0300

    feat: hanoi-tower fix

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,6 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   ret = {};
   ret.turns = Math.pow(2, disksNumber) - 1;
-  ret.seconds = turnsSpeed / ret.turns / 60 / 60;
+  ret.seconds = ret.turns / (turnsSpeed / 3600);
   return ret;
 };
