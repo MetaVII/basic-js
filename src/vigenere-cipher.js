ef77665cbe1602a492a755902fadf073868cb772
Alex <alex.kryzhny@mail.ru>
2020-03-02 21:32:32 +0300

    feat: vigenere-cipher solved

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,65 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
-    }
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
+  }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new TypeError();
     }
+    let ret = "";
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (j >= key.length) {
+        j = 0;
+      }
+      const charCode = message[i].toUpperCase().charCodeAt(0);
+      if (charCode < 65 || charCode > 90) {
+        ret += message[i];
+      } else {
+        ret += String.fromCharCode(
+          ((charCode - 65 + key[j].toUpperCase().charCodeAt() - 65) % 26) + 65
+        );
+        j++;
+      }
+    }
+    if (!this.isDirect) {
+      return ret
+        .split("")
+        .reverse()
+        .join("");
+    }
+    return ret;
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new TypeError();
+    }
+    let ret = "";
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (j >= key.length) {
+        j = 0;
+      }
+      const charCode = message[i].charCodeAt(0);
+      if (charCode < 65 || charCode > 90) {
+        ret += message[i];
+      } else {
+        ret += String.fromCharCode(
+          ((charCode - 65 - (key[j].toUpperCase().charCodeAt(0) - 65) + 26) %
+            26) +
+            65
+        );
+        j++;
+      }
+    }
+    if (!this.isDirect) {
+      return ret
+        .split("")
+        .reverse()
+        .join("");
+    }
+    return ret;
+  }
 }
 
 module.exports = VigenereCipheringMachine;
